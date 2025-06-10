2c9cec314d0f820db6e1266755f42c49a24f06b3
kiliman <kiliman@gmail.com>
2025-06-10 13:46:56 -0400

    chore(pixel-js): add build process and support scripts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  },
+  build: {
+    outDir: 'dist',
+    minify: false,
+    target: 'esnext',
+    // commonjsOptions: {
+    //   transformMixedEsModules: true,
+    // },
+    lib: {
+      name: 'pixel-v2',
+      formats: ['es'],
+      entry: './src/pixel-v2.js',
+      fileName: () => 'pixel-v2.js',
+    },
+  },
+});
