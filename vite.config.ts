650b20460b7bacad0a8f290fe6074e79fad07d07
Michael Carter <kiliman@gmail.com>
2025-08-27 13:39:35 -0400

    fix(pixel): ignore web crawlers (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-  },
-  build: {
-    outDir: 'dist',
-    minify: false,
-    target: 'esnext',
-    // commonjsOptions: {
-    //   transformMixedEsModules: true,
-    // },
-    lib: {
-      name: 'pixel_v2',
-      formats: ['iife'],
-      entry: './src/pixel-v2.js',
-      fileName: () => 'pixel-v2.js',
+export default defineConfig(({ mode }) => {
+  const entryName = process.env.VITE_BUILD_ENTRY || 'pixel-v2'; // Default to pixel-v2
+  const globalName = entryName.replace('-', '_'); // Convert pixel-v1 to pixel_v1 for global name
+  console.log(`Building ${entryName} with global name ${globalName} in ${mode} mode`);
+  return {
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     },
-  },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: false,
+      minify: false,
+      target: 'esnext',
+      lib: {
+        entry: `./src/${entryName}.js`,
+        formats: ['iife'],
+        name: globalName,
+        fileName: () => `${entryName}.js`,
+      },
+    },
+  };
 });
